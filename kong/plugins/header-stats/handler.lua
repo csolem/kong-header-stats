cbe6585270d7e1677b3a916dec70d9f00070a253
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-03-13 15:47:12 +0100

    ROR-240 Temporary log to nginx error log (TR-241)

--- kong/plugins/header-stats/handler.lua
+++ kong/plugins/header-stats/handler.lua
@@ -2,7 +2,7 @@
 local BasePlugin = require "kong.plugins.base_plugin"
 local req_get_headers = ngx.req.get_headers
 
--- local basic_serializer = require "kong.plugins.log-serializers.basic"
+local basic_serializer = require "kong.plugins.log-serializers.basic"
 
 local HeaderStatsHandler = BasePlugin:extend()
 HeaderStatsHandler.PRIORITY = 1
@@ -12,6 +12,7 @@ function HeaderStatsHandler:access(config)
   HeaderStatsHandler.super.access(self)
 
   local header_value = req_get_headers()[config.header_name]
+  nginx.log(ngx.ERR, "Got header value " .. header_value)
 
   print("Got header value " .. header_value .. " from header name " .. config.header_name)
 end
